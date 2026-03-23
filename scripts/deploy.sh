bb15cdb6ce2d6b09ec5470e776a05bab1fe877f7
Elliott Friedman <elliott.friedman@gmail.com>
2026-03-22 21:24:06 -0400

    [tmux] move tmux_statusline too

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
-#!/bin/bash -ex
+#!//usr/bin/zsh -ex
+
+SCRIPT_DIR=${${(%):-%x}:A:h}
 for name in bashrc tmux.conf vim vimrc zshrc; do
-  ln -svfT ${HOME}/.dotfiles/$name ${HOME}/.$name
+  ln -svfT "${SCRIPT_DIR}/../${name}" "${HOME}/.${name}"
 done
+ln -svfT "${SCRIPT_DIR}/tmux_statusline.sh" "${HOME}/.tmux_statusline.sh"
 
 touch ${HOME}/.zshrc.local
 
